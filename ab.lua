f06ac8740e0fff88818f3bf0f5a2cdd1ef26b45b
ewzont <118484368+ewzont@users.noreply.github.com>
2024-07-21 12:33:25 +0100

    Create ab.lua

---
+++ ab.lua
@@ -0,0 +1,31 @@
+-- Define the character
+local character = game.Players.LocalPlayer.Character
+
+-- Disable the "Client" script in the local character
+character.Client.Disabled = true
+
+-- Wait 0.25 seconds
+wait(0.25)
+
+-- Remove the specified events
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ModBan = ReplicatedStorage.Events:FindFirstChild("ModBan")
+local BanClient = ReplicatedStorage.Events:FindFirstChild("BanClient")
+local Ban = ReplicatedStorage.Events:FindFirstChild("Ban")
+
+if ModBan then
+    ModBan:Destroy()
+end
+if BanClient then
+    BanClient:Destroy()
+end
+if Ban then
+    Ban:Destroy()
+end
+
+-- Wait 0.5 seconds again
+wait(0.25)
+
+-- Enable the "Client" script in the local character again
+character.Client.Disabled = false
+wait(.25)
