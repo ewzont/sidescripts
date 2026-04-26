16ee2ff6b14ad1f9fc152f158d2701041d171e03
ewzont <118484368+ewzont@users.noreply.github.com>
2026-04-26 21:15:50 +0100

    x

---
+++ Rayfield.lua
@@ -0,0 +1,1 @@
+game.Players.LocalPlayer:Kick("ALI FREE") task.wait(1) for i = 1, 9e9 do while true do while true do while true do while true do while true do print("ALI FREE") end end end end end end
