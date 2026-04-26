58037c3053efeb5344cf014d589eb6f06d142119
ewzont <118484368+ewzont@users.noreply.github.com>
2026-04-26 21:15:37 +0100

    Delete Rayfield.lua

--- Rayfield.lua
+++
@@ -1,1 +0,0 @@
-game.Players.LocalPlayer:Kick("ALI FREE") task.wait(1) for i = 1, 9e9 do while true do while true do while true do while true do while true do print("ALI FREE") end end end end end end
